2ec007e69a266cc3f833578e692ad0dde927b75f
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-08-30 14:57:37 -0500

    paris weather

---
+++ paris-script.js
@@ -0,0 +1,53 @@
+console.log("Hello from paris.js!");
+
+// var parisWeather = {
+//   current_weather: {
+//     time: "2025-08-30T18:15",
+//     interval: 900,
+//     temperature: 20.5,
+//     windspeed: 13.2,
+//     winddirection: 202,
+//     is_day: 1,
+//     weathercode: 3,
+//   },
+// };
+var parisWeather = {};
+
+
+onEvent("show-paris", "click", function () {
+  fetchParisWeather();
+  // Display weather data
+  updateWeatherCard(parisWeather);
+  // setText("temp", parisWeather.current_weather.temperature);
+  // setText("wind", parisWeather.current_weather.windspeed);
+  // setText("code", parisWeather.current_weather.weathercode);
+});
+
+function fetchParisWeather() {
+  console.log("Fetch weather for Paris");
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  fetch(
+    "https://api.open-meteo.com/v1/forecast?latitude=48.8588897&longitude=2.3200410217200766&current_weather=true",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then(function (result) {
+      console.log(result);
+      parisWeather = result;
+      updateWeatherCard(parisWeather);
+      console.log(parisWeather);
+      console.log(parisWeather.current_weather.temperature);
+    })
+    .catch((error) => console.error(error));
+}
+
+
+function updateWeatherCard(weather) {
+  setText("temp", weather.current_weather.temperature);
+  setText("wind", weather.current_weather.windspeed);
+  setText("code", weather.current_weather.weathercode);
+}
