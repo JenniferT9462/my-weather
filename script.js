997c836280c0a4d6096144785f991998eb7b709e
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-08-30 14:56:58 -0500

    nola weather

--- script.js
+++ script.js
@@ -1,18 +1,58 @@
 console.log("Hello from script.js");
 
-var newOrleansWeather = {
-  latitude: 29.95,
-  longitude: -90.07,
-  current_weather: {
-    temperature: 31.2,
-    windspeed: 4.5,
-    weathercode: 1,
-  },
-};
+// var newOrleansWeather = {
+//     "current_weather": {
+//         "time": "2025-08-30T18:15",
+//         "interval": 900,
+//         "temperature": 28.0,
+//         "windspeed": 11.1,
+//         "winddirection": 347,
+//         "is_day": 1,
+//         "weathercode": 3
+//     }
+// };
 
-onEvent("searchBtn", "click", function () {
+var newOrleansWeather = {};
+// updateWeatherCard();
+
+
+
+onEvent("show-nola", "click", function () {
+  fetchNewOrleansWeather();
   // Display weather data
-  setText("temp", newOrleansWeather.current_weather.temperature);
-  setText("wind", newOrleansWeather.current_weather.windspeed);
-  setText("code", newOrleansWeather.current_weather.weathercode);
+  updateWeatherCard(newOrleansWeather);
+  // setText("temp", newOrleansWeather.current_weather.temperature);
+  // setText("wind", newOrleansWeather.current_weather.windspeed);
+  // setText("code", newOrleansWeather.current_weather.weathercode);
 });
+
+function fetchNewOrleansWeather() {
+  console.log("Fetch weather for New Orleans");
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  fetch(
+    "https://api.open-meteo.com/v1/forecast?latitude=29.95&longitude=-90.07&current_weather=true",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then(function (result) {
+      console.log(result);
+      newOrleansWeather = result;
+      updateWeatherCard(newOrleansWeather);
+      console.log(newOrleansWeather);
+      console.log(newOrleansWeather.current_weather.temperature);
+      console.log(newOrleansWeather.current_weather.weathercode);
+    })
+
+    .catch((error) => console.error(error));
+}
+
+
+function updateWeatherCard(weather) {
+  setText("temp", weather.current_weather.temperature);
+  setText("wind", weather.current_weather.windspeed);
+  setText("code", weather.current_weather.weathercode);
+}
